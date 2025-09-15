118700baba8a2d09e7013689a5466ef1690dba64
fivepm <fivepm.code@gmail.com>
2025-09-15 20:28:53 +0700

    update index

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ if (isset($_SESSION['user_id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - SIMAK Banguntapan 1</title>
-    <link rel="icon" type="image/png" href="assets/images/logo_kbm.png">
+    <link rel="icon" type="image/png" href="assets/images/logo_web_bg.png">
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <style>
--- users/ketuapjp/index.php
+++ users/ketuapjp/index.php
@@ -121,7 +121,7 @@ switch ($currentPage) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $pageTitle; ?> - Ketua PJP Panel</title>
-    <link rel="icon" type="image/png" href="../../assets/images/logo_kbm.png">
+    <link rel="icon" type="image/png" href="../../assets/images/logo_web_bg.png">
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
